8e384be23d439d684654b754ef2f3f643d0be37a
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-09-22 14:00:10 +0530

    more details class trigger

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
-  $("#more-details-btn").click(function () {
+  $(".more-details-btn").click(function () {
     $("#table-2").toggle("slow");
   });
 });
+
+const closeModal = (id) => {
+  document.getElementById(id).style.display = "none";
+};
